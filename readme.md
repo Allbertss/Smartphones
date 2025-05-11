ca90d7f597b77fb3c04743ea43dbcf7b3e56e032
Albertas Grigoryan <albertasgrigoryan@gmail.com>
2025-05-11 13:24:23 +0300

    docker environment

---
+++ readme.md
@@ -0,0 +1,28 @@
+# Smartphones
+
+## 📁 Project Structure
+
+- **[api](./api)**:
+- **[scraper](./scraper)**:
+- **[frontend](./frontend)**: 
+- **`docker-compose.yml`**:
+
+
+## 🚀 Running the Project
+
+To start the project locally using Docker, run the following command:
+
+```bash
+docker-compose up
+```
+
+To start scraper
+
+```bash
+docker compose run scraper python src/main.py
+```
+
+## 🌐 Access Points
+
+- **Frontend**: [http://localhost:3000](http://localhost:3000)
+- **API**: [http://localhost:8000/api](http://localhost:8000/api)
--- scraper/src/apiClient.py
+++ scraper/src/apiClient.py
@@ -1,6 +1,5 @@
 import requests
 
-
 class APIClient:
     def __init__(self, base_url, auth_token=None):
         self.base_url = base_url.rstrip("/")
@@ -19,14 +18,20 @@ class APIClient:
         error_count = 0
 
         for data in data_list:
-            response = requests.post(url, json=data, headers=self.headers, timeout=10)
-            
-            print(f"Response message: {response.text}")
-            print(f"Response status code: {response.status_code}")
+            try:
+                response = requests.post(url, json=data, headers=self.headers, timeout=10)
+                
+                print(f"Response message: {response.text}")
+                print(f"Response status code: {response.status_code}")
 
-            if response.status_code in (200, 201):
-                success_count += 1
-            else:
+                if response.status_code in (200, 201):
+                    success_count += 1
+                else:
+                    error_count += 1
+
+            except requests.exceptions.RequestException as e:
+                print(f"Request failed: {e}")
+                
                 error_count += 1
 
         return {
